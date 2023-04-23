a35d1f98b28577a2fcfa854cb801ae3c77ab68fa
Maxim <maks.bay.2015@mail.ru>
2023-04-23 22:28:46 +0300

    short models

--- app/src/main/java/com/example/githubusers/data/mappers/Mappers.kt
+++ app/src/main/java/com/example/githubusers/data/mappers/Mappers.kt
@@ -8,22 +8,11 @@ import com.example.githubusers.domain.models.UserDetail
 fun UserData.mapToUser(): User {
     return User(
         avatar_url = this.avatar_url,
-        events_url = this.events_url,
-        followers_url = this.followers_url,
-        following_url = this.following_url,
-        gists_url = this.gists_url,
-        gravatar_id = this.gravatar_id,
-        html_url = this.html_url,
         id = this.id,
         login  = this.login,
-        node_id = this.node_id,
         organizations_url = this.organizations_url,
-        received_events_url = this.received_events_url,
         repos_url = this.repos_url,
         site_admin = this.site_admin,
-        starred_url = this.starred_url,
-        subscriptions_url = this.subscriptions_url,
-        type = this.type,
         url = this.url
     )
 }
@@ -40,36 +29,13 @@ fun List<UserData>.mapToListUser(): List<User> {
 fun UserDetailData.mapToUserDetail(): UserDetail {
     return UserDetail(
         avatar_url = this.avatar_url,
-        bio = this.bio,
-        blog = this.blog,
-        company = this.company,
-        created_at = this.created_at,
         email = this.email,
-        events_url = this.events_url,
-        followers = this.followers,
-        followers_url = this.followers_url,
-        following = this.following,
-        following_url = this.following_url,
-        gists_url = this.gists_url,
-        gravatar_id = this.gravatar_id,
-        hireable = this.hireable,
-        html_url = this.html_url,
         id = this.id,
         location = this.location,
         login = this.login,
         name = this.name,
-        node_id = this.node_id,
         organizations_url = this.organizations_url,
-        public_gists = this.public_gists,
-        public_repos = this.public_repos,
-        received_events_url = this.received_events_url,
-        repos_url = this.repos_url,
-        site_admin = this.site_admin,
-        starred_url = this.starred_url,
-        subscriptions_url = this.subscriptions_url,
         twitter_username = this.twitter_username,
-        type = this.type,
-        updated_at = this.updated_at,
         url = this.url
     )
 }
